f22f9e9e74bd7fddcbc83fbcabf366d8e15dd9f8
Fumiya-Soeno <61449986+Fumiya-Soeno@users.noreply.github.com>
2020-03-05 12:02:51 +0900

    edit message.js to define "buildHTML"method and "done "method

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,46 @@
 $(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      var html =
+      `
+      <div class="main-chat__message-list__message">
+        <div class="main-chat__message-list__message__upper">
+          <div class="main-chat__message-list__message__upper__user">
+            ${message.user_name}
+          </div>
+          <div class="main-chat__message-list__message__upper__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="main-chat__message-list__message__main">
+          <div class="main-chat__message-list__message__main__contents">
+            ${message.content}
+          </div>
+        </div>
+        <img src=${message.image} >
+      </div>`
+      return html;
+    } else {
+      var html =
+      `
+      <div class="main-chat__message-list__message">
+        <div class="main-chat__message-list__message__upper">
+          <div class="main-chat__message-list__message__upper__user">
+            ${message.user_name}
+          </div>
+          <div class="main-chat__message-list__message__upper__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="main-chat__message-list__message__main">
+          <div class="main-chat__message-list__message__main__contents">
+            ${message.content}
+          </div>
+        </div>
+      </div>`
+      return html;
+    };
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,6 +52,11 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.main-chat__message-list').append(html);
+      $('form')[0].reset();
     })
   });
 });
