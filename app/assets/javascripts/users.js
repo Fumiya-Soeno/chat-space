29eda4fa3fd85066178c1104ce02801b97280e16
Fumiya-Soeno <61449986+Fumiya-Soeno@users.noreply.github.com>
2020-03-06 15:05:47 +0900

    edit users.js and _form.html.haml to add incremental-serch function

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -15,9 +15,21 @@ $(function(){
       <div class="chat-group-user clearfix">
         <p class="chat-group-user__name">ユーザーが見つかりません</p>
       </div>
-      `;
+      `
       return html;
     }
+  }
+  function addDeleteUser(name, id) {
+    let html = `
+    <div class="chat-group-user clearfix" id="${id}">
+      <p class="chat-group-user__name">${name}</p>
+      <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</div>
+    </div>`;
+    $(".js-add-user").append(html);
+  }
+  function addMember(userId) {
+    let html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
+    $(`#${userId}`).append(html);
   }
   $("#user-search-field").on("keyup", function(){
     var input = $("#user-search-field").val();
@@ -40,4 +52,19 @@ $(function(){
       alert("ユーザー検索に失敗しました");
     });
   })
+  $(document).on("click", ".chat-group-user__btn--add", function() {
+    console.log
+    const userName = $(this).attr("data-user-name");
+    const userId = $(this).attr("data-user-id");
+    $(this)
+      .parent()
+      .remove();
+    addDeleteUser(userName, userId);
+    addMember(userId);
+  });
+  $(document).on("click", ".chat-group-user__btn--remove", function() {
+    $(this)
+      .parent()
+      .remove();
+  });
 });
